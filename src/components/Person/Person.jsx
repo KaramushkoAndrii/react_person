5aade01275bb08cefbf221ca8df448fd07baf134
Andii Karamushko <karamushko1997@gmail.com>
2026-05-05 17:14:57 +0300

    fix solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,13 +2,15 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
+  const second = sex === 'm' ? 'wife' : 'husband';
+
   return (
     <section className="Person">
-      <h2 className="Person__name">{name}</h2>
+      <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
       {isMarried ? (
         <p className="Person__partner">
-          {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
+          {partnerName} is my {second}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
